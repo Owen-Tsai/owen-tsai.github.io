d30caf73b75ae617b242817a5d02303eff3ff6ba
Owen Tsai <owentsai.v@gmail.com>
2025-01-31 16:49:36 +0800

    some fix, idk

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,6 @@ import { resolve } from 'node:path'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  ssr: false,
   devtools: { enabled: true },
   nitro: {
     prerender: {
--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -2,21 +2,23 @@
   <article class="prose prose-neutral dark:prose-invert">
     <h1>Blog</h1>
 
-    <ContentList path="/posts" :query="query" v-slot="{ list }">
-      <div v-for="[year, entry] in processList(list)" :key="year" class="relative mb-8">
-        <div
-          class="text-8xl text-stroke-2 text-stroke-neutral-200 dark:text-stroke-neutral-700 text-transparent font-900 mb-4 relative h-8 -z-1"
-        >
-          <span class="absolute -left-8 top-0">{{ year }}</span>
+    <ContentList path="/posts" :query="query">
+      <template v-slot="{ list }">
+        <div v-for="[year, entry] in processList(list)" :key="year" class="relative mb-8">
+          <div
+            class="text-8xl text-stroke-2 text-stroke-neutral-200 dark:text-stroke-neutral-700 text-transparent font-900 mb-4 relative h-8 -z-1"
+          >
+            <span class="absolute -left-8 top-0">{{ year }}</span>
+          </div>
+          <PostEntry
+            v-for="post in entry"
+            :key="post._path"
+            :title="post.title"
+            :date="post.date"
+            :to="post._path"
+          />
         </div>
-        <PostEntry
-          v-for="post in entry"
-          :key="post._path"
-          :title="post.title"
-          :date="post.date"
-          :to="post._path"
-        />
-      </div>
+      </template>
     </ContentList>
 
     <div class="mt-20">
