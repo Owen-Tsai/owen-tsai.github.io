5bb6f37c96e964d7e47a86686ef4a30882ff0111
Owen Tsai <owentsai.v@gmail.com>
2026-03-29 00:43:07 +0800

    add workaround for process not exit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,9 +2,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  hooks: {
+    'build:done': () => {
+      setTimeout(() => {
+        process.exit(0)
+      }, 5000)
+    },
+  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  ignore: ['pages/**/*.ts'],
   modules: ['@nuxt/content', '@nuxt/eslint', '@vueuse/nuxt', '@nuxt/image'],
   vite: {
     plugins: [tailwindcss()],
