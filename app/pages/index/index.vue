f3b26c4a7290c930377b88c63fb88b7a74365b67
Owen Tsai <owentsai.v@gmail.com>
2026-03-31 11:46:38 +0800

    refactor: update font

--- app/pages/index/index.vue
+++ app/pages/index/index.vue
@@ -2,15 +2,18 @@
   <div class="w-screen h-screen flex flex-col justify-end p-6 md:p-12">
     <div ref="canvas" class="absolute top-0 left-0 w-full h-full canvas-wrapper"></div>
     <div class="h-full max-w-4xl z-10 flex flex-col justify-center">
-      <h2 class="text-zinc-500 text-sm tracking-widest mb-4 font-mono">new Sr?.webDeveloper();</h2>
+      <h2 class="text-zinc-500 text-sm tracking-widest mb-8 font-monospaced">
+        // new Sr?.webDeveloper();
+      </h2>
       <h1
-        class="text-zinc-50 text-5xl md:text-7xl lg:text-8xl font-light tracking-tighter leading-none"
+        class="text-zinc-50 text-5xl md:text-7xl lg:text-8xl font-light tracking-tighter leading-[0.85] font-display"
       >
-        Engineering<br />
-        <span class="italic text-zinc-400">Atmospheric</span><br />
-        Experiences.
+        ENGINEERING<br />
+        <span class="italic text-zinc-400 font-artistic align-super lg:text-9xl">atmospheric</span
+        ><br />
+        EXPERIENCES.
       </h1>
-      <p class="mt-8 text-zinc-400 max-w-md text-sm md:text-base leading-relaxed">
+      <p class="mt-8 text-zinc-500 max-w-md text-sm md:text-base leading-relaxed">
         Focusing on the intersection of design, motion, and robust engineering architecture.
         Crafting interfaces that feel alive.
       </p>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,18 +2,29 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  hooks: {
-    'build:done': () => {
-      setTimeout(() => {
-        process.exit(0)
-      }, 5000)
-    },
-  },
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@vueuse/nuxt', '@nuxt/image'],
+  modules: ['@nuxt/content', '@nuxt/eslint', '@vueuse/nuxt', '@nuxt/image', '@nuxt/fonts'],
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [
+        'lucide-vue-next',
+        'gsap',
+        'gsap/dist/ScrambleTextPlugin', // CJS
+        'three',
+        'lodash-es',
+        'three/addons/loaders/FontLoader.js',
+        'three/addons/geometries/TextGeometry.js',
+      ],
+    },
+  },
+  fonts: {
+    families: [
+      { name: 'Space Grotesk', weight: 300 },
+      { name: 'Cormorant Garamond', weight: 300 },
+      { name: 'Space Mono', weight: [300, 500] },
+    ],
   },
   nitro: {
     prerender: {
